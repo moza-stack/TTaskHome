444050c310a4e3163987d9d89b43af2812e0d62b
Mozam <moza.m.almazidi@gmail.com>
2026-04-29 12:49:52 +0400

    dictionary

---
+++ soluation15/Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Dictionary<string, int> studentData = new Dictionary<string, int>();
+            studentData.Add("Ali", 25);
+            studentData.Add("Moza", 27);
+            try
+            {
+                Console.WriteLine($"Ali is " + studentData["Ali"] + "years old"); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
